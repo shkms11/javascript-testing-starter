75fa46d26ee599e2e60ebb8d0b8e132f83ae0936
Shamsul <48136141+shkms11@users.noreply.github.com>
2024-07-18 15:21:11 +0600

    Revert "refactor"
    This reverts commit abc6a10017d847020a489d93ef5d41cff4d6d5a0.

--- src/intro.js
+++ src/intro.js
@@ -1,12 +1,14 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-    return a > b ? a : b;
+  if (a > b) return a;
+  else if (b > a) return b;
+  return a;
 }
 
 // Exercise
 export function fizzBuzz(n) {
-    if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
-    if (n % 3 === 0) return "Fizz";
-    if (n % 5 === 0) return "Buzz";
-    return n.toString();
+  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+  if (n % 3 === 0) return 'Fizz';
+  if (n % 5 === 0) return 'Buzz';
+  return n.toString();
 }
